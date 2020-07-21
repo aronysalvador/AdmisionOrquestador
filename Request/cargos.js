c7276bae6c877e18569c8a946044796f6f46c7ae
Tony <tony.krikorian@outlook.com>
2020-07-21 17:23:11 -0400

    Add cargos request

---
+++ Request/cargos.js
@@ -0,0 +1,11 @@
+const getCargos = () => {
+  return {
+    url: "",
+    headers: {
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+      },
+    },
+  };
+};
+module.exports = getCargos;
