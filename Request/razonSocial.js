d03f96518600b8af9bb2f43e70d0e4a84301c1b9
Díaz Gareca <sadiazg@achs.cl>
2020-08-24 23:21:17 -0400

    razon social

--- Request/razonSocial.js
+++ Request/razonSocial.js
@@ -1,13 +1,13 @@
 const getRazonSocial = (companyName) => {
   return {
-    url: `https://wa-desa-bd.azurewebsites.net/api/razonSocial?companyName=${companyName}`,
+    url: `https://wa-desa-bd.azurewebsites.net/api/razonSocial/getByName?companyName=${companyName}`,
     headers: {
       "Content-Type": "application/json; charset=utf-8",
     },
   };
 };
 
-const getRazonSocialByRut = (companyName) => {
+const getRazonSocialByRut = (rut) => {
   return {
     url: `https://wa-desa-bd.azurewebsites.net/api/razonSocial/getByRut?rut=${rut}`,
     headers: {
