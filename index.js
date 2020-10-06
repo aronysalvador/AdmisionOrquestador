e83a351998aee12561dbaf530e56377aadb7cc3c
Sebastian Cristi <secristic@ext.achs.cl>
2020-10-06 08:44:08 -0400

    Se aumenta limite de recepcion de documentos debido a documento base64

--- index.js
+++ index.js
@@ -40,7 +40,8 @@ app.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 });
  */
 
-app.use(express.json());
+app.use(express.json({limit: '9mb'}));
+app.use(express.urlencoded({limit: '9smb', extended: true}));
 app.use(morgan("dev"));
 app.use(cors());
 app.use("/api/sap", sap);
